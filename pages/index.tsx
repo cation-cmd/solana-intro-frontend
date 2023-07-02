f1cc0a868f27a15aabe56f5185cf8879c827ee6a
Kingsley Amadi <akingsley655@gmail.com>
2023-07-02 11:12:47 -0400

    My Solana App Correction

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,23 @@
-import type { NextPage } from "next"
-import { useState } from "react"
-import styles from "../styles/Home.module.css"
-import AddressForm from "../components/AddressForm"
-import * as anchor from "@project-serum/anchor"
+import type { NextPage } from 'next'
+import { useState } from 'react'
+import styles from '../styles/Home.module.css'
+import AddressForm from '../components/AddressForm'
+import * as Web3 from '@solana/web3.js'
 
 const Home: NextPage = () => {
   const [balance, setBalance] = useState(0)
   const [address, setAddress] = useState('')
-  const [isExecutable, setIsExecutable] = useState(true);
 
   const addressSubmittedHandler = (address: string) => {
     try {
       setAddress(address)
-      const key = new anchor.web3.PublicKey(address)
-      const connection = new anchor.web3.Connection(
-        anchor.web3.clusterApiUrl("devnet")
-      )
-      connection.getBalance(key).then((balance) => {
-        setBalance(balance / anchor.web3.LAMPORTS_PER_SOL)
+      const key = new Web3.PublicKey(address)
+      const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'))
+      connection.getBalance(key).then(balance => {
+        setBalance(balance / Web3.LAMPORTS_PER_SOL)
       })
     } catch (error) {
-      setAddress("")
+      setAddress('')
       setBalance(0)
       alert(error)
     }
@@ -29,11 +26,12 @@ const Home: NextPage = () => {
   return (
     <div className={styles.App}>
       <header className={styles.AppHeader}>
-        <p>Start Your Solana Journey</p>
+        <p>
+          Start Your Solana Journey
+        </p>
         <AddressForm handler={addressSubmittedHandler} />
         <p>{`Address: ${address}`}</p>
         <p>{`Balance: ${balance} SOL`}</p>
-        <p>{`Is it executable? ${isExecutable ? 'Yep' : 'Nope'}`}</p>
       </header>
     </div>
   )
